ab3eb74ab48b8e4ecfb448c99f778a23183db7ad
FelipeCruz777 <felipe.souza@bandtec.com.br>
2021-05-30 00:12:13 -0300

    Atualização

--- Sprint1-projeto/Site/API-projeto-site/models/usuario.js
+++ Sprint1-projeto/Site/API-projeto-site/models/usuario.js
@@ -1,41 +1,40 @@
 	'use strict';
 
-/* 
-lista e explicação dos Datatypes:
-https://codewithhugo.com/sequelize-data-types-a-practical-guide/
-*/
+	/* 
+	lista e explicação dos Datatypes:
+	https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+	*/
 
-module.exports = (sequelize, DataTypes) => {
-    let Usuario = sequelize.define('Usuario',{
-		id: {
-			field: 'id',
-			type: DataTypes.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},		
-		nome: {
-			field: 'nome',
-			type: DataTypes.STRING,
-			allowNull: false
-		},
-		login_backend: {
-			field: 'login_bd',
-			type: DataTypes.STRING,
-			allowNull: false
-		},
-		
-		senha: {
-			field: 'senha',
-			type: DataTypes.STRING,
-			allowNull: false
-		}
-	}, 
-	{
-		tableName: 'usuario_bd', 
-		freezeTableName: true, 
-		underscored: true,
-		timestamps: false,
-	});
+	module.exports = (sequelize, DataTypes) => {
+	    let Cadastro = sequelize.define('Cadastro', {
+	        idUsuario: {
+	            field: 'idUsuario',
+	            type: DataTypes.INTEGER,
+	            primaryKey: true,
+	            autoIncrement: true
+	        },
+	        nomeUsuario: {
+	            field: 'nomeUsuario',
+	            type: DataTypes.STRING,
+	            allowNull: false
+	        },
+	        emailUsuario: {
+	            field: 'emailUsuario',
+	            type: DataTypes.STRING,
+	            allowNull: false
+	        },
 
-    return Usuario;
-};
+	        senha: {
+	            field: 'senha',
+	            type: DataTypes.STRING,
+	            allowNull: false
+	        }
+	    }, {
+	        tableName: 'cadastro',
+	        freezeTableName: true,
+	        underscored: true,
+	        timestamps: false,
+	    });
+
+	    return Cadastro;
+	};
